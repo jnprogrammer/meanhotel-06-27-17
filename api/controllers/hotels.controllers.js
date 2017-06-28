03e0085d3b5e782e1d644ddbaabf8d261216a6c1
jnprogrammer <joshuanparker@gmail.com>
2017-06-27 23:29:44 -0400

    Section one completed correctly

---
+++ api/controllers/hotels.controllers.js
@@ -0,0 +1,41 @@
+var hotelData = require('../data/hotel-data.json');
+
+module.exports.hotelsGetAll = function(req, res){
+     console.log("Get hotel data!!");
+     console.log("QUERY:", req.query);
+
+     var offset = 0;
+     var count = 5;
+
+     if(req.query && req.query.offset){
+         offset = parseInt(req.query.offset,10);
+     }
+
+    if(req.query && req.query.count){
+         count = parseInt(req.query.count,10);
+     }
+
+     var returnData = hotelData.slice(offset,offset + count);
+
+    res
+        .status(200)
+        .json(returnData);
+};
+
+module.exports.hotelsGetOne = function(req, res){
+
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+     console.log("Get a hotel ID!!");
+    res
+        .status(200)
+        .json(thisHotel);
+};
+
+module.exports.hotelsAddOne = function(req, res){
+     console.log("POST new hotel !!");
+     console.log(req.body);
+    res
+        .status(200)
+        .json(req.body);
+};
--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var routes = require('./api/routes');
+var bodyParser = require('body-parser');
 
 app.set('port',3000);
+
+//Logging middle ware, must be above all res and req code
+app.use(function(req,res,next){
+    console.log(req.method,req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({extended:false})); //setting to true can give access to different data types
+
+app.use('/api',routes);
+
 
 var server = app.listen(app.get('port'),function(){
     var port = server.address().port;
     console.log("mean things are happening on port" + port);
 });
 
-console.log('me first');
+console.log('me first, showing asynchronous functionality');
