04fcf99af7627c79638528e2bad803a2e440e22e
jnprogrammer <joshuanparker@gmail.com>
2017-07-10 20:00:21 -0400

    Started building front end in angular

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# meanhotel-06-27-17
+# meanhotel-06-27-17 Completed 07-10-17
 A fullstack project using the MEAN stack
+
--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('./api/data/db.js');
 var express = require('express');
 var app = express();
 var path = require('path');
-var routes = require('./api/routes');
 var bodyParser = require('body-parser');
+
+var routes = require('./api/routes');
+
 
 app.set('port',3000);
 
@@ -14,6 +16,9 @@ app.use(function(req,res,next){
 });
 
 app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/node_modules',express.static(__dirname + '/node_modules'));
+
 app.use(bodyParser.urlencoded({extended:false})); //setting to true can give access to different data types
 
 app.use('/api',routes);
